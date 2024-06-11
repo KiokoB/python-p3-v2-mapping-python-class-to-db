3cb57f52ab9851a9530cd10efa76bdb0b22822ac
Bianca Kioko <kiokobianca01@gmail.com>
2024-06-11 14:23:38 +0300

    orm: create/drop table, save, update or delete data

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,17 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+hr = Department.create("Human Resources", "Building C, East Wing")
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll) 
+
 ipdb.set_trace()
